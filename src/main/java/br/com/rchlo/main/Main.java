461ba7d5ef0509570b6f9cf1779790d9465c850c
sruiNascimento <sruy19@gmail.com>
2023-01-02 15:34:54 -0300

    refatoracao da pr: filtro de camisetas, equals adicionado, get removido e orElseThrow adicionado

--- src/main/java/br/com/rchlo/main/Main.java
+++ src/main/java/br/com/rchlo/main/Main.java
@@ -1,52 +1,64 @@
 package br.com.rchlo.main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import br.com.rchlo.domain.Cor;
 import br.com.rchlo.domain.ListaDeProdutos;
 import br.com.rchlo.domain.Produto;
+import br.com.rchlo.domain.Tamanho;
 
 public class Main {
     public static void main(String[] args) {
         List<Produto> camisas = ListaDeProdutos.lista();
 
-        System.out.println("Apresentando o nome de todas as camisas");
-        camisas.forEach(camisa -> System.out.println(camisa.getNome()));
+        //Mostrar todas as camisetas
+        List<Produto> camisetas =  camisas.stream()
+            .filter(camisa -> camisa.getNome().toLowerCase().contains("camiseta"))
+            .collect(Collectors.toList());
+        
+        System.out.println("Apresentando o nome de todas as camisetas");
+        camisetas.forEach(camiseta -> System.out.println(camiseta.getNome()));
 
-        System.out.println("Apresentando o nome de todas as camisas brancas");
-        camisas.stream()
-            .filter(camisa -> camisa.getCor() == Cor.BRANCA)
+        //Mostrar todas as camisetas
+        System.out.println("Apresentando as camisetas brancas");
+        camisetas.stream()
+            .filter(camisa -> camisa.getCor().equals(Cor.BRANCA))
             .forEach(camisa -> System.out.println(camisa.getNome()));     
-        // Apresentando todos detalhes
-            // .forEach(System.out::println);   
-        
-        
+
+        //Mostrar se existe ou não alguma camiseta cinza
         boolean temAlgumaCamisaCinza = camisas.stream()
-            .anyMatch(camisa -> camisa.getCor() == Cor.CINZA);
+            .anyMatch(camisa -> camisa.getCor().equals(Cor.CINZA));
         System.out.println("Há alguma camisa cinza? " + temAlgumaCamisaCinza);
 
+        //Mostrar o menor preço de um produto que contenha desconto
         Produto camisaMenorValorComDesconto = camisas.stream()
             .filter(Produto::temDesconto)
             .min((camisa1, camisa2) -> camisa1.getPreco().compareTo(camisa2.getPreco()))
-            .get();
+            .orElseThrow(() -> new RuntimeException("Produto não encontrado!"));
         
         System.out.println("Menor preço das camisas que tem desconto RS " + camisaMenorValorComDesconto.getPreco()); 
         
-        Produto camisaMaiorValorComDesconto = camisas.stream()
-            .filter(Produto::temDesconto)
+        //Mostrar o produto que tenha o maior preço
+        Produto produtoMaiorPreco = camisas.stream()
+            // .filter(Produto::temDesconto)
             .max((camisa1, camisa2) -> camisa1.getPreco().compareTo(camisa2.getPreco()))
-            .get();
-        
-        System.out.println("Maior preço das camisas que tem desconto RS " + camisaMaiorValorComDesconto.getPreco()); 
+            .orElseThrow(() -> new RuntimeException("Produto não encontrado!"));
 
-        Map<Cor, Long> cores = camisas.stream()
+        System.out.println("Produto com o maior preço RS " + produtoMaiorPreco.getPreco()); 
+
+        //Mostrar todas as cores com a quantidade de produtos de cada cor
+        Map<Cor, Long> coresPorQuantidade = camisas.stream()
             .collect(Collectors.groupingBy(Produto::getCor, Collectors.counting()));
-        
-        cores.forEach((cor, quantidade) -> System.out.println("cor = " + cor + " quantidade " + quantidade));
+        System.out.println("Cor - Quantidade");
+        coresPorQuantidade.forEach((cor, quantidade) -> System.out.printf("cor: %-8s - quantidade: %d%n", cor, quantidade));
 
-        //falta eu apresentar os tamanhos com os produtos de cada tamanho, esse eu preciso estudar mais um pouquinho para realizar              
+        //Mostrar os tamanhos com os produtos de cada tamanho              
+        //Não consegui fazer :'(
 
     }
 }
